6e67e9835bb04e02a126e994f2ce73d4fb94f0c3
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-21 13:01:50 +0300

    Add files via upload

---
+++ sinav3-847/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace sinav3_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSayi = new System.Windows.Forms.Button();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSayi
+            // 
+            this.btnSayi.Location = new System.Drawing.Point(88, 121);
+            this.btnSayi.Name = "btnSayi";
+            this.btnSayi.Size = new System.Drawing.Size(159, 73);
+            this.btnSayi.TabIndex = 0;
+            this.btnSayi.Text = "rastgele sayı oluştur";
+            this.btnSayi.UseVisualStyleBackColor = true;
+            this.btnSayi.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(336, 139);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(35, 13);
+            this.lblSonuc.TabIndex = 1;
+            this.lblSonuc.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.btnSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSayi;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ sinav3-847/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinav3_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sayi = random.Next(0, 101);
+
+            lblSonuc.Text = sayi.ToString();
+
+            if ( sayi < 50)
+            {
+                lblSonuc.ForeColor = Color.Red;
+            }
+            else if ( sayi >= 50 && sayi <= 70)
+            {
+                lblSonuc.ForeColor = Color.Orange;
+            }
+            else
+            {
+                lblSonuc.ForeColor= Color.Blue;
+            }
+
+        }
+    }
+}
